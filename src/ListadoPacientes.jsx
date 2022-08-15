14ff7a89b99109354ead0007697eec5c19a7b9a7
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-08-15 10:11:43 -0400

    creando Listado de Pacientes

--- src/ListadoPacientes.jsx
+++ src/ListadoPacientes.jsx
@@ -1,7 +1,32 @@
 const ListadoPacientes = () => {
     return (
         <div className="w-1/2 lg:w-3/5">
-            <h2>Listado Pacientes</h2>
+            <h2 className="font-black text-3xl text-center  ">Listado Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+                Administra tus {''}
+                <span className="text-indigo-600 font-bold  ">Pacientes y Citas</span>
+            </p>
+            <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+                <p className="font-bold mb-3 text-gray-700 uppercase " >Nombre: {''}
+                    <span className="font-normal normal-case">Hook</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase " >Propietario: {''}
+                    <span className="font-normal normal-case">Carol</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase " >Email: {''}
+                    <span className="font-normal normal-case">correo@correo.com</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase " >Fecha de Alta: {''}
+                    <span className="font-normal normal-case">15 de Agosto de 2022</span>
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase " >Síntomas: {''}
+                    <span className="font-normal normal-case">Sólo Duerme</span>
+                </p>
+            </div>
         </div>
     )
 }
