5cc8cf28442b9b3c39a2d67a01ff1e9c21f472e2
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-08-18 23:51:44 -0400

    Eliminar Paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,6 @@
-const Paciente = ({paciente, setPaciente}) => {
-    
-    const {nombre, propietario, email, fecha, sintomas} = paciente
+const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+
+    const { nombre, propietario, email, fecha, sintomas, id } = paciente
 
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -26,12 +26,13 @@ const Paciente = ({paciente, setPaciente}) => {
             <div className="flex justify-between mt-10">
                 <button
                     type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg" 
-                    onClick={()=> setPaciente(paciente)}                
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => setPaciente(paciente)}
                 >Editar</button>
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => eliminarPaciente(id)}
                 >Eliminar</button>
             </div>
         </div>
